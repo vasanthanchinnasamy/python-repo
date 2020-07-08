983b5b6cc0dbe7389e767b75c9eae790b4e5f65e
Vasanthan <vasanthantamilan@gmail.com>
2020-07-08 11:40:53 +0530

    git check123

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/index')
 def index():
-    return '<h1>Vasanthan Deployment from Git Check</h1>'
+    return '<h1>Vasanthan Deployment from Git Check 123</h1>'
 
 if __name__ == '__main__':
 	app.run(debug=True)
